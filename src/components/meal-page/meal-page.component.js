63db71c2d41d44342d2e91cdae2458208db551a6
Joao-Matias <jdmatias@live.com>
2023-04-26 20:54:15 +0100

    Code MealPage was getting to cluttered. Refactored some code to make it slimmer adding a new component

--- src/components/meal-page/meal-page.component.js
+++ src/components/meal-page/meal-page.component.js
@@ -3,7 +3,7 @@ import style from './meal-page.module.css';
 import listImg from '../../img/list.jpg';
 import MealForm from '../meal-form';
 import MealRecipeDetail from '../meal-recipe-detail';
-import { ImCross } from 'react-icons/im';
+import RecipeList from '../recipe-list';
 
 import { Link } from 'react-router-dom';
 
@@ -13,35 +13,10 @@ const MealPage = () => {
   const [selectedRecipe, setSelectedRecipe] = useState();
 
   const [listOfRecipes, setListOfRecipes] = useState([]);
-  const [deleteRecipeModal, setDeleteRecipeModal] = useState(false);
-  const [recipeToDelete, setRecipeToDelete] = useState();
 
   const toggleForm = () => {
     setOpenFormModal(true);
     setSelectedRecipe(false);
-  };
-
-  const handleClickRecipe = (recipe) => {
-    setOpenFormModal(false);
-    setSelectedRecipe(recipe);
-  };
-
-  const deleteRecipe = (recipe) => {
-    setDeleteRecipeModal(true);
-    setRecipeToDelete(recipe);
-  };
-
-  const handleDeleteRecipe = (recipe) => {
-    console.log(recipe);
-    setListOfRecipes((prevState) => {
-      const listOfRecipes = prevState.filter((rec) => {
-        return rec.id !== recipe.id;
-      });
-      return listOfRecipes;
-    });
-
-    setSelectedRecipe('');
-    setDeleteRecipeModal(false);
   };
 
   return (
@@ -51,51 +26,13 @@ const MealPage = () => {
           Start a Recipe
         </button>
         <ul className={style.recipesContainer}>
-          {listOfRecipes.map((recipe, i) => {
-            return (
-              <li key={i} className={style.recipes}>
-                {(deleteRecipeModal && recipeToDelete.id === recipe.id && (
-                  <div className={style.deleteModal}>
-                    <div className={style.deleteModalQuestion}>
-                      <h3>Are you sure you want to delete:</h3>
-                      <h2>{recipe.mealName}?</h2>
-                    </div>
-                    <h5
-                      onClick={() => {
-                        handleDeleteRecipe(recipe, i);
-                      }}
-                    >
-                      Yes
-                    </h5>
-                    <h5>No</h5>
-                  </div>
-                )) || (
-                  <>
-                    <button
-                      onClick={() => {
-                        handleClickRecipe(recipe);
-                      }}
-                      className={
-                        selectedRecipe.id === recipe.id
-                          ? style.selectedRecipe
-                          : style.recipesBtn
-                      }
-                    >
-                      {recipe.mealName}
-                    </button>
-                    <i
-                      className={style.deleteBox}
-                      onClick={() => {
-                        deleteRecipe(recipe);
-                      }}
-                    >
-                      <ImCross className={style.delete} />
-                    </i>
-                  </>
-                )}
-              </li>
-            );
-          })}
+          <RecipeList
+            listOfRecipes={listOfRecipes}
+            setOpenFormModal={setOpenFormModal}
+            setSelectedRecipe={setSelectedRecipe}
+            setListOfRecipes={setListOfRecipes}
+            selectedRecipe={selectedRecipe}
+          />
         </ul>
       </div>
 
---
+++ src/components/recipe-list/recipe-list.component.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { useState } from 'react';
+
+import { ImCross } from 'react-icons/im';
+import style from './recipe-list.module.css';
+
+const RecipeList = (props) => {
+  const {
+    listOfRecipes,
+    setOpenFormModal,
+    setSelectedRecipe,
+    setListOfRecipes,
+    selectedRecipe,
+  } = props;
+
+  const [recipeToDelete, setRecipeToDelete] = useState();
+  const [deleteRecipeModal, setDeleteRecipeModal] = useState(false);
+
+  const handleClickRecipe = (recipe) => {
+    setOpenFormModal(false);
+    setSelectedRecipe(recipe);
+  };
+
+  const deleteRecipe = (recipe) => {
+    setDeleteRecipeModal(true);
+    setRecipeToDelete(recipe);
+  };
+
+  const handleDeleteRecipe = (recipe) => {
+    setListOfRecipes((prevState) => {
+      const listOfRecipes = prevState.filter((rec) => {
+        return rec.id !== recipe.id;
+      });
+      return listOfRecipes;
+    });
+
+    setSelectedRecipe('');
+    setDeleteRecipeModal(false);
+  };
+
+  const closeDeleteModule = () => {
+    setRecipeToDelete('');
+    setDeleteRecipeModal(false);
+  };
+
+  return listOfRecipes.map((recipe, i) => {
+    return (
+      <li key={i} className={style.recipes}>
+        {(deleteRecipeModal && recipeToDelete.id === recipe.id && (
+          <div className={style.deleteModal}>
+            <div className={style.deleteModalQuestion}>
+              <h3>Are you sure you want to delete:</h3>
+              <h2>{recipe.mealName}?</h2>
+            </div>
+            <h5
+              onClick={() => {
+                handleDeleteRecipe(recipe, i);
+              }}
+            >
+              Yes
+            </h5>
+            <h5
+              onClick={() => {
+                closeDeleteModule(recipe);
+              }}
+            >
+              No
+            </h5>
+          </div>
+        )) || (
+          <>
+            <button
+              onClick={() => {
+                handleClickRecipe(recipe);
+              }}
+              className={
+                selectedRecipe.id === recipe.id
+                  ? style.selectedRecipe
+                  : style.recipesBtn
+              }
+            >
+              {recipe.mealName}
+            </button>
+            <i
+              className={style.deleteBox}
+              onClick={() => {
+                deleteRecipe(recipe);
+              }}
+            >
+              <ImCross className={style.delete} />
+            </i>
+          </>
+        )}
+      </li>
+    );
+  });
+};
+
+export default RecipeList;
