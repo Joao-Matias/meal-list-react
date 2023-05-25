13212ad315a24b77afd2e17fe94d2462861bac42
Joao-Matias <jdmatias@live.com>
2023-05-25 18:51:42 +0100

    Add some elements on the new lists page and some logic to record the input value and store it

--- src/components/list-nav/list-nav.component.js
+++ src/components/list-nav/list-nav.component.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './list-nav.module.css';
+import { ImPlus } from 'react-icons/im';
+import './list-nav.module.css';
 
 const ListNav = () => {
+  const [openInputNewItem, setOpenInputNewItem] = useState(false);
+  const [updateName, setUpdateName] = useState(``);
+  const [newElement, setNewElement] = useState([]);
+
+  const addNewLine = () => {
+    setOpenInputNewItem(true);
+  };
+
+  const clickToAddNew = (keyDown) => {
+    if (keyDown.key === 'Enter') {
+      setNewElement([updateName]);
+      setOpenInputNewItem(false);
+    }
+  };
+
   return (
     <section className={style.listContainerBox}>
-      <h1>Let's Build Your Shopping Lists!</h1>
+      <h1 className={style.title}>Let's Build Your Shopping Lists!</h1>
       <nav className={style.listNav}>
         <button>Lists</button>
-        <input className={style.addNew} placeholder='Add New and press enter' />
+        <div onClick={addNewLine} className={style.addNewBox}>
+          {openInputNewItem ? (
+            <input
+              onKeyDown={(event) => {
+                clickToAddNew(event);
+              }}
+              onChange={(event) => {
+                setUpdateName(event.target.value);
+              }}
+              autoFocus
+              placeholder='Press enter to confirm'
+            />
+          ) : (
+            <h3>Add New</h3>
+          )}
+
+          <ImPlus className={style.addNew} />
+        </div>
       </nav>
     </section>
   );
